46b70064a3cd271b08d7eda55a4ce162d1aad86e
Huber Palomino <huber.palomino@gdpteam.com>
2018-07-24 18:24:39 -0500

    add .go

---
+++ helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "io"
+  "net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "<body style='background-color: red'>Hello World!</body>")
+}
+
+func main() {
+  http.HandleFunc("/", hello)
+  http.ListenAndServe(":80", nil)
+}
+
